9dfcff5975e79074dafb3476eb05225c966caed2
WildWinter <69979179+TGRDD@users.noreply.github.com>
2024-05-04 17:03:42 +0300

    Add multimeterScroll effect
    Animated scrolling

--- Assets/InternalAssets/Code/Systems/Interactables/MultimeterScrollerInteractable.cs
+++ Assets/InternalAssets/Code/Systems/Interactables/MultimeterScrollerInteractable.cs
@@ -8,6 +8,7 @@ public class MultimeterScrollerInteractable : MonoBehaviour, IInteractable
     public static event Action<int> OnScrollStateChanged;
     public UnityEvent OnInteract;
     public UnityEvent OnInteractionLeft;
+    public UnityEvent OnSwitchSucces;
 
     [SerializeField] private int _currentScrollState;
 
@@ -18,20 +19,19 @@ public class MultimeterScrollerInteractable : MonoBehaviour, IInteractable
 
     private int _maxScrollPosition => _scrollStatesZrotationArray.Length - 1;
     private Vector3 _currentRotation => _scrollObjectTransform.rotation.eulerAngles;
+    private bool _clockwiseRotation;
+    private bool isRotating;
+
     private void OnValidate()
     {
         _scrollObjectTransform ??= GetComponent<Transform>();
-
-        if (_scrollStatesZrotationArray.Length > 0)
-        {
-            ClampIfOutOfRange();
-            RotateScrollObject();
-        }
     }
 
     public void Interact(InteractAction action)
     {
         OnInteract?.Invoke();
+
+        if (isRotating) { return; }
         switch (action)
         {
             case InteractAction.ScrollUp: _currentScrollState++; break;
@@ -41,9 +41,11 @@ public class MultimeterScrollerInteractable : MonoBehaviour, IInteractable
         }
 
         ClampIfOutOfRange();
+
+        _clockwiseRotation = action == InteractAction.ScrollUp ? true : false;
         RotateScrollObject();
 
-        OnScrollStateChanged?.Invoke(_currentScrollState);
+        OnSwitchSucces?.Invoke();
     }
 
     public void LeaveInteraction()
@@ -60,8 +62,25 @@ public class MultimeterScrollerInteractable : MonoBehaviour, IInteractable
 
     private void RotateScrollObject()
     {
-        Vector3 newRotation = _currentRotation;
-        newRotation.z = _scrollStatesZrotationArray[_currentScrollState];
-        _scrollObjectTransform.rotation = Quaternion.Euler(newRotation);
+        StartCoroutine(RotateObjectCoroutine(_scrollStatesZrotationArray[_currentScrollState]));
+    }
+
+    System.Collections.IEnumerator RotateObjectCoroutine(float targetZ)
+    {
+        isRotating = true;
+        float currentRotation = _currentRotation.z;
+
+        for (int i = 0; i <= 10; i++)
+        {
+
+            currentRotation = Mathf.Lerp(currentRotation, targetZ, 0.5f);
+            transform.rotation = Quaternion.Euler(_currentRotation.x, _currentRotation.y, currentRotation);
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.rotation = Quaternion.Euler(_currentRotation.x, _currentRotation.y, targetZ);
+        isRotating = false;
+
+        OnScrollStateChanged?.Invoke(_currentScrollState);
     }
 }
